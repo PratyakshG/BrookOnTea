13697512cbf48d770ce98d2d8782e55b1b4e2eb8
Pratyaksh Gupta <pincodeazm@gmail.com>
2025-08-10 18:36:37 +0530

    check

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,8 +5,10 @@ const Navbar = () => {
         offers of the day!!!
       </div>
       <nav className="w-full h-[10vh] flex max-lg:flex-col bg-white items-center justify-between px-6 py-2 lg:px-12 lg:py-5 sticky z-10 top-0">
-        <h1 className="font-bold text-3xl lg:text-5xl font-bricolage">BrookOnTea</h1>
-        <ul className="flex items-center max-lg:py-2 gap-3 lg:gap-8 *:text-xs *:lg:text-base cursor-pointer">
+        <h1 className="font-bold text-3xl lg:text-5xl font-bricolage">
+          BrookOnTea
+        </h1>
+        <ul className="flex items-center max-lg:py-2 gap-3 lg:gap-8 *:text-xs *:lg:text-base cursor-pointer text-red-500">
           <li>Home</li>
           <li>About</li>
           <li>Gallery</li>
